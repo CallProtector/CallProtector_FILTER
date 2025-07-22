02b77bffc127e32eb02fa2fe78034ed8a1203116
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-07-22 18:49:44 +0900

    🐛 Fix: Fix: app.py root 경로 수정 (springboot 충돌 방지 위해)
    Fix: app.py root 경로 수정 (springboot 충돌 방지 위해)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from detect.detect import is_abuse
 
-app = FastAPI(root_path="/filter-abuse")
+app = FastAPI() # ✅ springboot 경로 충돌을 방지하기 위해 제거
 
 class AbuseRequest(BaseModel):
     text: str
